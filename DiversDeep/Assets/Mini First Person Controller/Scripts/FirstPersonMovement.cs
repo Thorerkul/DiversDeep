552f2264a9769ec5ec65fe9a00c2aebb8e075628
T - TA <thoukkol@gmail.com>
2023-08-24 15:13:04 +0200

    final commit, cancelling project

---
+++ DiversDeep/Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (speedOverrides.Count > 0)
+        {
+            speed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity =new Vector2( Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+
+        // Apply movement.
+        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+    }
+}
--- DiversDeep/Assets/PlayerScript.cs
+++ DiversDeep/Assets/PlayerScript.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public GameObject mesh;
+    [Header("References")]
+    public CharacterController controller;
+    public Transform cameraTransform;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 2f;
+    public float turnSpeed = 0.1f;
+
+    private Vector3 velocity;
+    public bool isUnderwater;
 
     private void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        CameraControl();
 
-        Debug.Log(input);
+        //isUnderwater = CheckUnderwaterStatus();
+
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+
+        velocity = input.normalized * moveSpeed;
+
+        velocity += Physics.gravity;
+
+        transform.Translate(velocity * Time.deltaTime, Space.Self);
+
+        controller.Move(new Vector3(0,0,0));
+    }
+
+    void CameraControl()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("RightJoystickHorizontal"), 0, Input.GetAxisRaw("RightJoystickVertical"));
+        Vector3 velocity2 = input.normalized * turnSpeed;
+        Debug.Log(velocity2);
+
+        cameraTransform.rotation = Quaternion.Euler(cameraTransform.rotation.eulerAngles.x + velocity2.x, cameraTransform.rotation.eulerAngles.y, cameraTransform.rotation.eulerAngles.z);
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + velocity2.z, transform.rotation.z);
+        controller.transform.Rotate(new Vector3(0, 0-velocity2.z, 0), Space.Self);
+    }
+
+    private bool CheckUnderwaterStatus()
+    {
+        return false;
     }
 }
